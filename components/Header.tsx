3dbf1c48b6bbee700ccaea40897c9901e815993f
Alanolog <alanrutyna2003@gmail.com>
2023-02-20 20:26:23 +0100

    header done

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function Header() {
   return (
-    <header>
+    <header className=" flex justify-between p-5 max-w-7xl mx-auto">
       <div className="flex items-center space-x-5">
         <Link href="/">
           <img
@@ -15,12 +15,17 @@ function Header() {
         <div className="hidden md:inline-flex items-center space-x-5">
           <h3>About</h3>
           <h3>Contact</h3>
-          <h3 className=" text-white bg-green-500 px-4 py-1 rounded-full">
+          <h3 className=" text-white bg-green-700 px-4 py-1 rounded-full">
             Follow
           </h3>
         </div>
       </div>
-      <div></div>
+      <div className="flex items-center space-x-5  text-green-700 ">
+        <h3>Sign In</h3>
+        <h3 className="border px-4 rounded-full border-green-700">
+          Get Started
+        </h3>
+      </div>
     </header>
   );
 }
