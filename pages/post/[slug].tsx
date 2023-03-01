fee77fe22bc1950e9af6081a5560fffd6d0ff224
Alanolog <alanrutyna2003@gmail.com>
2023-03-01 11:18:29 +0100

    staticPaths

---
+++ pages/post/[slug].tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Header from "../../components/Header";
+import { sanityClient, urlFor } from "../../sanity";
+import { Post } from "../../typings";
+
+const Post = () => {
+  return (
+    <main>
+      <Header />
+    </main>
+  );
+};
+
+export default Post;
+
+export const getStaticPaths = async () => {
+  const query = `
+    *[_type == "post"]{
+        _id,
+        slug {
+          current
+        }
+      }
+    `;
+  const posts = await sanityClient.fetch(query);
+  const paths = posts.map((post: Post) => ({
+    params: {
+      slug: post.slug.current,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: "blocking",
+  };
+};
