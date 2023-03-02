36c8b3f41e7ea92b837704276596ef890cc99118
Alanolog <alanrutyna2003@gmail.com>
2023-03-02 18:18:01 +0100

    add comments to article

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -19,7 +19,7 @@ interface Props {
 
 const Post = ({ post }: Props) => {
   const [submitted, setSubmitted] = React.useState(false);
-  console.log(post);
+
   const {
     register,
     handleSubmit,
@@ -157,6 +157,18 @@ const Post = ({ post }: Props) => {
           />
         </form>
       )}
+      <div className="flex flex-col p-10 my-10 max-w-2xl mx-auto  shadow-yellow-500 shadow space-y-2">
+        <h3 className="text-4xl">Comments</h3>
+        <hr className="pb-2" />
+        {post.comments.map((comment) => (
+          <div key={comment._id}>
+            <p>
+              <span className="text-yellow-500">{comment.name}: </span>
+              {comment.comment}
+            </p>
+          </div>
+        ))}
+      </div>
     </main>
   );
 };
